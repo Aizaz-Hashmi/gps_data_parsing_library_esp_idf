ee86daba3c3550284a457aa50bd06aca33efefc5
Aizaz Hashmi <aizazubaid@gmail.com>
2024-07-13 09:12:48 +0500

    Update gps_data_parser.h
    added time struct, and add all data members of GGA sentence to show all data from GGA sentence.

--- components/gps_data_parser/include/gps_data_parser.h
+++ components/gps_data_parser/include/gps_data_parser.h
@@ -13,28 +13,67 @@
 #ifndef GPS_DATA_PARSER_H
 #define GPS_DATA_PARSER_H
 
+#include <stdint.h>
 
 /**
- * @brief Structure to store GPS data.
- *
- * This structure contains parsed GPS data from a UART stream, including time,
- * latitude, longitude, and altitude. The time is in the HHMMSS.SSS format,
- * latitude and longitude are in degrees and minutes (DDMM.MMMM format for latitude
- * and DDDMM.MMMM format for longitude), and altitude is above sea level in meters.
+ * @brief Structure to store GPS time.
  */
 typedef struct {
-    // Time in HHMMSS.SSS format
-    char time[13];
+    uint8_t hour;      /*!< Hour (00-23) */
+    uint8_t minute;    /*!< Minute (00-59) */
+    uint8_t second;    /*!< Second (00-59) */
+    uint16_t thousand; /*!< Thousandths of a second (000-999) */
+} gps_time_t;
 
-    // Latitude in DDMM.MMMM format
-    char latitude[15];
+/**
+ * @brief Structure to store parsed GPS data.
+ *
+ * This structure contains parsed GPS data from a GGA Sentence, including time,
+ * latitude, longitude, altitude, fix quality, number of satellites, HDOP, and geoid height.
+ * The time is in the HHMMSS.SSS format, latitude and longitude are in degrees and minutes 
+ * (DDMM.MMMM format for latitude and DDDMM.MMMM format for longitude), altitude and geoid 
+ * height are above sea level in meters, and fix quality, number of satellites, and HDOP 
+ * are also provided.
+ */
+typedef struct {
+    // Time in HHMMSS.SSS format from GGA sentence
+    gps_time_t time;
+
+    // Latitude in DDMM.MMMM format from GGA sentence 
+    float latitude;
+
+    // Latitude direction (N/S)
+    char lat_direction;
 
     // Longitude in DDDMM.MMMM format
-    char longitude[15];
+    float longitude;
+
+    // Longitude direction (E/W)
+    char lon_direction;
+
+    // Fix quality (0 = invalid, 1 = GPS fix, 2 = DGPS fix)
+    int fix_quality;
+
+    // Number of satellites being tracked , Range is 0 to 12
+    int num_satellites;
+
+    // Horizontal Dilution of Precision (indicating the accuracy of a GPS receiver's horizontal position (latitude and longitude)) Lower value means more accuracy.
+    float hdop;
 
     // Altitude above sea level in meters
-    char altitude[12];
+    float altitude;
+
+    // Units of altitude (M)
+    char altitude_units;
+
+    // Geoid separation (difference between WGS-84 earth ellipsoid and mean sea level)
+    float geoid_height;
+
+    // Units of geoid separation (M)
+    char geoid_height_units;
+
 } gps_data_parse_t;
+
 
 /**
  * @brief  Declaration of function to Parse GPS data from a UART stream.
@@ -47,6 +86,6 @@ typedef struct {
  *
  * @return Parsed GPS data in a gps_data_parse_t structure.
  */
-gps_data_parse_t gps_data_parser(const char * uart_stream);
+gps_data_parse_t* gps_data_parser(const char * uart_stream);
 
 #endif  // GPS_DATA_PARSER_H
