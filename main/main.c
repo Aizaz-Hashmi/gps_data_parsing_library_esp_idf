f9b423a2cf5ff54a78d24c62b4e8a470f7ad887b
Aizaz Hashmi <aizazubaid@gmail.com>
2024-07-18 14:48:37 +0500

    Update main.c
    Added a pointer object to gps_data_parse_t struct.

--- main/main.c
+++ main/main.c
@@ -29,17 +29,18 @@ void test(const char * stream, int stream_num);
  *
  * @return int Returns 0 on successful execution of the application.
  */
-int app_main() {
 
 
-	while(1) {
 
-	// using test cases
-    const char *stream1 = "";// empty string, invalid stream
+void app_main(void)
+{  while(1) {
+
+    // using test cases
+    const char *stream1 = "$GPGGA,,,,13258.3334,W,,8,1.03,,M,,M,,*31\r\n";// empty string, invalid stream
     test(stream1,1);
     sleep(1);//1 second delay using POSIX API
 
-    const char *stream2 = "$GPGGA,002153.000,1585.7149,N,07737.2052,E,1,4,5.57,333.2,M,-88.5,M,,*70\r\n";// pure GPGGA sentence with CRLF
+   const char *stream2 = "$GPGGA,002153.000,1585.7149,N,07737.2052,E,1,4,5.57,333.2,M,-88.5,M,,*70\r\n";// pure GPGGA sentence with CRLF
     test(stream2,2);
     sleep(1);
 
@@ -51,18 +52,20 @@ int app_main() {
     test(stream4,4);
     sleep(1);
 
-    const char *stream5 = ",09275,,,,,,,,,,ewrwer,sdfvzx,453453635,41,,,A*43\r\n$GPGGA,092751.000,5321.6802,N,00630.3371,W,1,8,1.03,61.7,M,55.3,M,,*75\r\n	$GPGSA,A,3,10,07,05,02,29,04,08,13,,,,,1.72,1.03,1.38*0A\r\n$GPGSV,AIZAZ UABID HASHMI 	59,290,20,08,54$GPGSV,3,2,11,02,39,223,16,13,28,070,17,26,SDFJDSNFJ23,252,,04,1SDJFNASJDKFSDJHDS4,186,15*77\r\n	$GPGSV,3,3,11,29,09,3JDSAFNJKDASFDSC01,24,16,09,020,,36,,,*76\r\n";// corrupted packet sample but uncorrupted GGA sent by  GPS module to mcu through UART
+    const char *stream5 = ",09275,,,,,,,,,,ewrwer,sdfvzx,453453635,41,,,A*43\r\n$GPGGA,092751.000,5321.6802,N,00630.3371,W,1,8,1.03,61.7,M,55.3,M,,*75\r\n  $GPGSA,A,3,10,07,05,02,29,04,08,13,,,,,1.72,1.03,1.38*0A\r\n$GPGSV,AIZAZ UABID HASHMI   59,290,20,08,54$GPGSV,3,2,11,02,39,223,16,13,28,070,17,26,SDFJDSNFJ23,252,,04,1SDJFNASJDKFSDJHDS4,186,15*77\r\n $GPGSV,3,3,11,29,09,3JDSAFNJKDASFDSC01,24,16,09,020,,36,,,*76\r\n";// corrupted packet sample but uncorrupted GGA sent by  GPS module to mcu through UART
      test(stream5,5);
      sleep(1);
 
    // Now testing  when stream is valid , and it is NMEA sentence and even checksum result is true but some data parameters are missing
    // when time parameter is missing due to some issue but checksum attached with is without time field empty so checksum will be true
-     const char * stream6 = "$GPGGA,,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*61\r\n"; // time is missing
+     const char * stream6 = "$GPGGA,,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,3,123*62\r\n"; // time is missing
      test(stream6,6);
      sleep(1);
+     
      const char * stream7 = "$GPGGA,,,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*46\r\n"; // latitude  is also missing
      test(stream7,7);
      sleep(1);
+     
      const char * stream8 = "$GPGGA,,,N,,W,1,8,1.03,61.7,M,55.2,M,,*58\r\n"; // longitude  is also missing
      test(stream8,8);
      sleep(1);
@@ -74,11 +77,17 @@ int app_main() {
      test(stream10,10);
      sleep(1);
      const char * stream11 = ",,,$$%%&^*(*(@,,,,4531313372,W,0.02,31.66,280511,,,A*43\r\n$GPGGA,065551.680,5321.6802,S,00630.3371,E,1,85,,,,554.3,M,,*30\r\n$GPGSA,A,3,10,07,05##(())(,1.38*0A\r%^&*()_+n$GPGSV,,./??3,1,11,10,63,137,17,07,61,098,15,05,59,290,20,08,54,157,30*70\r";//highly corrupted stream
-     test(stream11,11);
+     test(stream11,11); 
+     const char * stream12 = "$GPGGA,abcdefgh,,N,12345.67,E,1,08,1.0,10.0,M,0.0,M,18,ABC*09\r\n";
+     test(stream12,11);
      sleep(1);
-	}
-    return 0;
+      
+     
+
+    }
 }
+   
+
 
 /**
  * @brief Tests the parsing of a GPS data stream.
@@ -90,13 +99,29 @@ int app_main() {
  */
 void test(const char* stream, int stream_num)
 {
+    gps_gga_handle_t data = NULL;
+   data =  gps_data_parser(stream);
 
-	    gps_data_parse_t data = gps_data_parser(stream);
+    // Check if memory allocation failed and data is NULL
+    if (data == NULL) {
+        ESP_LOGE(TAG, "Memory allocation failed for GPS data parser.");
+        return;
+    }
 
-	    // Using ESP-IDF logging function for each output
-	    ESP_LOGI(TAG, "stream no. %d TIME: %s",stream_num, data.time);
-	    ESP_LOGI(TAG, "stream no. %d LATITUDE: %s",stream_num, data.latitude);
-	    ESP_LOGI(TAG, "stream no. %d LONGITUDE: %s", stream_num,data.longitude);
-	    ESP_LOGI(TAG, "stream no. %d ALTITUDE: %s\n",stream_num, data.altitude);
+    
+
+    // Using ESP-IDF logging function for each output
+    ESP_LOGI(TAG, "stream no. %d TIME: %02d:%02d:%02d.%d", stream_num, data->time.hour, data->time.minute, data->time.second, data->time.millisecond);
+    ESP_LOGI(TAG, "stream no. %d LATITUDE: %f %c", stream_num, data->latitude, data->lat_direction);
+    ESP_LOGI(TAG, "stream no. %d LONGITUDE: %f %c", stream_num, data->longitude, data->lon_direction);
+    ESP_LOGI(TAG, "stream no. %d FIX QUALITY: %d", stream_num, data->fix_quality);
+    ESP_LOGI(TAG, "stream no. %d NUMBER OF SATELLITES: %d", stream_num, data->num_satellites);
+    ESP_LOGI(TAG, "stream no. %d HORIZONTAL DILUTION: %0.3f", stream_num, data->hdop);
+    ESP_LOGI(TAG, "stream no. %d ALTITUDE: %0.3f %c", stream_num, data->altitude, data->altitude_units);
+    ESP_LOGI(TAG, "stream no. %d GEOIDAL SEPARATION: %0.3f %c", stream_num, data->geoid_height, data->geoid_height_units);
+    ESP_LOGI(TAG, "stream no. %d AGE OF DIFFERENTIAL GPS DATA: %f", stream_num, data->dgps_age);
+    ESP_LOGI(TAG, "stream no. %d DIFFERENTIAL REFERENCE STATION ID: %d", stream_num, data->dgps_station_id);
+    
+    free(data);
 
 }
